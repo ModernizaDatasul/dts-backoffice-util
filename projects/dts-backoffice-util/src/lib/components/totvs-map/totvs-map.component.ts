bd660635f2678294d45eaff23dc8e98300a65404
Adriano de Avila <adriano.avila@totvs.com.br>
2025-01-31 14:37:27 -0300

    Correção para apresentar corretamente as cores nos diversos cenários dos parâmetros de lista de estados e estado inicial secionado (informados através de variável o fixo no html).

--- projects/dts-backoffice-util/src/lib/components/totvs-map/totvs-map.component.ts
+++ projects/dts-backoffice-util/src/lib/components/totvs-map/totvs-map.component.ts
@@ -1,39 +1,29 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, QueryList, Renderer2, SimpleChanges, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, Output, QueryList, Renderer2, SimpleChanges, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-totvs-map',
   templateUrl: './totvs-map.component.html',
   styleUrls: ['./totvs-map.component.css']
 })
-export class TotvsMapComponent implements OnChanges {
+export class TotvsMapComponent implements AfterViewInit, OnChanges {
   literals: any = {};
 
   @ViewChildren("anchorStateElement") mapLinks: QueryList<ElementRef>;
 
-  @Input({required: false}) enabledStates!: string[];
-  @Input({required: false}) initialSelectedState!: string;
+  @Input({ required: false }) enabledStates!: string[];
+  @Input({ required: false }) initialSelectedState!: string;
   @Output() selectedStateEvent = new EventEmitter<string>();
-
 
   constructor(
     private renderer: Renderer2
   ) { }
 
+  ngAfterViewInit(): void {
+    this.initStyleOnState();
+  }
+
   ngOnChanges(simpleChanges: SimpleChanges) {
-    if (this.enabledStates) {
-      this.mapLinks?.forEach((anchor: ElementRef) => {
-        const anchorElement: HTMLElement = anchor.nativeElement;
-        const stateElement: Element = anchorElement.children[anchorElement.children.length - 1];
-        if (!this.enabledStates?.includes(stateElement.innerHTML)) {
-          this.disableState(anchorElement, stateElement);
-        } else {
-          this.enableState(anchorElement, stateElement);
-        }
-      });
-    }
-    if (this.initialSelectedState) {
-      this.selectedState(this.initialSelectedState);
-    }
+    this.initStyleOnState();
   }
 
   enableState(parentElement: HTMLElement, stateElement: Element): void {
@@ -68,18 +58,38 @@ export class TotvsMapComponent implements OnChanges {
     } else {
       this.removeClassStyles(anchorElement, 'defaultState');
     }
-    
+
     this.renderer.addClass(anchorElement.children[0], stateStyle)
     if (anchorElement.children.length > 2) { // Estados pequenos com o círculo
       this.renderer.addClass(anchorElement.children[1], stateStyle + 'Circle');
-    } 
+    }
 
     this.renderer.addClass(anchorElement, 'stateWithText');
     this.renderer.addClass(anchorElement.children[anchorElement.children.length - 1], 'text')
   }
 
+  initStyleOnState(): void {
+    this.mapLinks?.forEach((anchor: ElementRef) => {
+      const anchorElement: HTMLElement = anchor.nativeElement;
+      const stateElement: Element = anchorElement.children[anchorElement.children.length - 1];
+
+      if (this.enabledStates && !this.enabledStates.includes(stateElement.innerHTML)) {
+        this.disableState(anchorElement, stateElement);
+      } else {
+        this.enableState(anchorElement, stateElement);
+      }
+
+      this.applyStyleOnState(anchor.nativeElement, true);
+    });
+
+    this.selectedState(this.initialSelectedState);
+  }
+
   selectedState(state: string): void {
-    this.mapLinks.forEach((anchor: ElementRef) => {
+    if (!state) { return; }
+    if (!this.mapLinks) { return; }
+
+    this.mapLinks?.forEach((anchor: ElementRef) => {
       const anchorElement: HTMLElement = anchor.nativeElement;
       const stateElement: Element = anchorElement.children[anchorElement.children.length - 1];
       this.applyStyleOnState(anchorElement, stateElement.innerHTML !== state);
