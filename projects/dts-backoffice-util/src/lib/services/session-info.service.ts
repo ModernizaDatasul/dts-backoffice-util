8820be4f873dd34e6b08c2bf8f37cdda7ca8fa73
Adriano de Avila <adriano.avila@totvs.com.br>
2026-09-03 18:42:58 -0300

    UserLoginService: Alterado para buscar o usuário corrente de sessionStorage.

--- projects/dts-backoffice-util/src/lib/services/session-info.service.ts
+++ projects/dts-backoffice-util/src/lib/services/session-info.service.ts
@@ -1,32 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ISessionInfo } from '../model/session-info.model';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserLoginService {
-
-    readonly apiUrl = '/totvs-menu/rest/getSessionInfo';
-    userLogin = undefined;
-
     constructor(public http: HttpClient) { }
 
     getUserLogin(): Observable<string> {
+        if (sessionStorage.getItem('username')) {
+            return of(sessionStorage.getItem('username'));
+        }
+
         if (localStorage.getItem('username')) {
             return of(localStorage.getItem('username'));
         }
 
-        if (this.userLogin) {
-            return of(this.userLogin);
-        } else {
-            return this.http.get<ISessionInfo>(`${this.apiUrl}`)
-                .pipe(map((response) => {
-                    this.userLogin = response.userName;
-                    return response.userName;
-                }));
-        }
+        return of(undefined);
     }
 }
